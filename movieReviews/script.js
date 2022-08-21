1fd50d335a8caf56e78a96fa7aae3a55dc7d0a12
Chandan2485 <chandan96kar@gmail.com>
2022-08-21 18:57:39 +0530

    data_store

--- movieReviews/script.js
+++ movieReviews/script.js
@@ -1,40 +1,291 @@
 let allArray=[
-    {img:"hdfdff", 
-    name: "gdfgdfgd",},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {}
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    },
+    {
+        ID:1,
+        img:"hdfdff", 
+        name: "gdfgdfgd",
+        catagory:"DFUYHSG",
+        year:2020,
+        Rateing:8.1
+    }
 ]
+

b6a0f9d8394fb08106c145facff7136a1b093b3a
Chandan2485 <chandan96kar@gmail.com>
2022-08-22 00:46:28 +0530

    data

--- movieReviews/script.js
+++ movieReviews/script.js
@@ -1,291 +1,400 @@
 let allArray=[
     {
         ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
+        img:"img/2020/t/Don't Listen.jpg", 
+        name: "Don't Listen",
+        catagory:"Thriller",
+        year:2020,
+        Rateing:6.1
+    },
+    {
+        ID:2,
+        img:"img/2020/a/Tenet.jpg", 
+        name: "Tenet",
+        catagory:"Action",
         year:2020,
         Rateing:8.1
     },
     {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
+        ID:3,
+        img:"img/2020/a/Time to Hunt.jpg", 
+        name: "Time to Hunt",
+        catagory:"Action",
+        year:2020,
+        Rateing:6.3
+    },
+    {
+        ID:4,
+        img:"img/2020/a/Birds of Prey.jpg", 
+        name: "Birds of Prey",
+        catagory:"Action",
+        year:2020,
+        Rateing:6.3
+    },
+    {
+        ID:5,
+        img:"img/2020/a/The Old Guard.jpg", 
+        name: "The Old Guard",
+        catagory:"Action",
+        year:2020,
+        Rateing:6.6
+    },
+    {
+        ID:6,
+        img:"img/2020/c/THE HIGH NOTE.jpg", 
+        name: "THE HIGH NOTE",
+        catagory:"Comedy",
+        year:2020,
+        Rateing:7.7
+    },
+    {
+        ID:7,
+        img:"img/2020/c/STARGIRL.jpg", 
+        name: "STARGIRL",
+        catagory:"Comedy",
+        year:2020,
+        Rateing:6.9
+    },
+    {
+        ID:8,
+        img:"img/2020/c/AN AMERICAN PICKLE.jpg", 
+        name: "AN AMERICAN PICKLE",
+        catagory:"Comedy",
+        year:2020,
+        Rateing:7.1
+    },
+    {
+        ID:9,
+        img:"img/2020/c/BUFFALOED.jpg", 
+        name: "BUFFALOED",
+        catagory:"Comedy",
         year:2020,
         Rateing:8.1
     },
     {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
+        ID:10,
+        img:"img/2020/t/Cadaver.jpg", 
+        name: "Cadaver",
+        catagory:"Thriller",
+        year:2020,
+        Rateing:6.9
+    },
+    {
+        ID:11,
+        img:"img/2020/t/His House.jpg",
+        name: "His House",
+        catagory:"Thriller",
+        year:2020,
+        Rateing:6.5
+    },
+    {
+        ID:12,
+        img:"img/2020/t/The Empty Man.jpg", 
+        name: "The Empty Man",
+        catagory:"Thriller",
+        year:2020,
+        Rateing:7.7
+    },
+    {
+        ID:47,
+        img:"img/2020/h/Unhinged.webp", 
+        name: "Unhinged",
+        catagory:"Horror",
         year:2020,
         Rateing:8.1
     },
     {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
+        ID:48,
+        img:"img/2020/h/Samhain.jpg", 
+        name: "Samhain",
+        catagory:"Horror",
         year:2020,
+        Rateing:6.4
+    },
+    {
+        ID:45,
+        img:"img/2020/h/Scientist.jpg", 
+        name: "Scientist",
+        catagory:"Horror",
+        year:2020,
+        Rateing:8.2
+    },
+    {
+        ID:46,
+        img:"img/2020/h/45.jpg", 
+        name: "45",
+        catagory:"Horror",
+        year:2020,
+        Rateing:7.1
+    },
+
+
+
+
+
+
+
+    {
+        ID:13,
+        img:"img/2021/t/A Stalker in the House.jpg", 
+        name: "A Stalker in the House",
+        catagory:"Thriller",
+        year:2021,
+        Rateing:7.4
+    },
+    {
+        ID:14,
+        img:"img/2021/t/Devils-Island.jpg", 
+        name: "Devils-Island",
+        catagory:"Thriller",
+        year:2021,
+        Rateing:7.1
+    },
+    {
+        ID:15,
+        img:"img/2021/t/hide and seek.jpg", 
+        name: "hide and seek",
+        catagory:"Thriller",
+        year:2021,
+        Rateing:6.5
+    },
+    {
+        ID:16,
+        img:"img/2021/t/Motherly.jpg", 
+        name: "Motherly",
+        catagory:"Thriller",
+        year:2021,
+        Rateing:7.7
+    },
+    {
+        ID:17,
+        img:"img/2021/c/Addams Family 2.jpg", 
+        name: "Addams Family 2",
+        catagory:"Comedy",
+        year:2021,
+        Rateing:6.1
+    },
+    {
+        ID:18,
+        img:"img/2021/c/interview.jpg", 
+        name: "interview",
+        catagory:"Comedy",
+        year:2021,
+        Rateing:8.0
+    },
+    {
+        ID:19,
+        img:"img/2021/c/War of the Roses.jpg", 
+        name: "War of the Roses",
+        catagory:"Comedy",
+        year:2021,
+        Rateing:7.3
+    },
+    {
+        ID:20,
+        img:"img/2021/c/Ye-Mard-Bechara.jpg", 
+        name: "Ye Mard Bechara",
+        catagory:"Comedy",
+        year:2021,
+        Rateing:6.0
+    },
+    {
+        ID:21,
+        img:"img/2021/a/Jurassic hunt.jpg", 
+        name: "Jurassic hunt",
+        catagory:"Action",
+        year:2021,
+        Rateing:8.3
+    },
+    {
+        ID:22,
+        img:"img/2021/a/kings man.jpg", 
+        name: "kings man",
+        catagory:"Action",
+        year:2021,
         Rateing:8.1
     },
     {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
+        ID:23,
+        img:"img/2021/a/Tomorrow war.jpg", 
+        name: "Tomorrow war",
+        catagory:"Action",
+        year:2021,
+        Rateing:7.2
+    },
+    {
+        ID:24,
+        img:"img/2021/a/Without Remorse.png", 
+        name: "Without Remorse",
+        catagory:"Action",
+        year:2021,
+        Rateing:7.5
+    },
+    {
+        ID:41,
+        img:"img/2021/h/Screams.jpg", 
+        name: "Screams",
+        catagory:"Horror",
+        year:2021,
+        Rateing:7.0
+    },
+    {
+        ID:42,
+        img:"img/2021/h/Separation.jpg", 
+        name: "Separation",
+        catagory:"Horror",
+        year:2021,
+        Rateing:6.9
+    },
+    {
+        ID:43,
+        img:"img/2021/h/while we sleep.jpg", 
+        name: "while we sleep",
+        catagory:"Horror",
+        year:2021,
+        Rateing:8.0
+    },
+    {
+        ID:44,
+        img:"img/2021/h/WrongTurn.jpg", 
+        name: "WrongTurn",
+        catagory:"Horror",
+        year:2021,
+        Rateing:6.1
+    },
+
+
+
+
+
+    {
+        ID:25,
+        img:"img/2022/a/Ambulance.jpg", 
+        name: "Ambulance",
+        catagory:"Action",
+        year:2022,
         Rateing:8.1
     },
     {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
+        ID:26,
+        img:"img/2022/a/DHAAKAD.jpg", 
+        name: "DHAAKAD",
+        catagory:"Action",
+        year:2022,
+        Rateing:6.5
+    },
+    {
+        ID:27,
+        img:"img/2022/a/The-Amityville-Uprising.jpg", 
+        name: "The Amityville Uprising",
+        catagory:"Action",
+        year:2022,
+        Rateing:5.9
+    },
+    {
+        ID:28,
+        img:"img/2022/a/UNCHARTED.jpg", 
+        name: "UNCHARTED",
+        catagory:"Action",
+        year:2022,
+        Rateing:7.7
+    },
+    {
+        ID:29,
+        img:"img/2022/c/father of the bride.jpg", 
+        name: "Father of The Bride",
+        catagory:"Comedy",
+        year:2022,
+        Rateing:6.9
+    },
+    {
+        ID:30,
+        img:"img/2022/c/I-Want-You-Back.webp", 
+        name: "I Want You Back",
+        catagory:"Comedy",
+        year:2022,
+        Rateing:7.9
+    },
+    {
+        ID:31,
+        img:"img/2022/c/Mother Schmuckers.jpg", 
+        name: "Mother Schmuckers",
+        catagory:"Comedy",
+        year:2022,
+        Rateing:6.5
+    },
+    {
+        ID:32,
+        img:"img/2022/c/The-Duke.jpg", 
+        name: "The Duke",
+        catagory:"Comedy",
+        year:2022,
+        Rateing:6.7
+    },
+    {
+        ID:33,
+        img:"img/2022/t/Beyond Existence.jpg", 
+        name: "Beyond Existence",
+        catagory:"Thriller",
+        year:2022,
+        Rateing:7.3
+    },
+    {
+        ID:34,
+        img:"img/2022/t/Dual.jpg", 
+        name: "Dual",
+        catagory:"Thriller",
+        year:2022,
+        Rateing:6.4
+    },
+    {
+        ID:35,
+        img:"img/2022/t/Take-the-Night.jpg", 
+        name: "Take The Night",
+        catagory:"Thriller",
+        year:2022,
+        Rateing:7.9
+    },
+    {
+        ID:36,
+        img:"img/2022/t/Unsheltered.jpg", 
+        name: "Unsheltered",
+        catagory:"Thriller",
+        year:2022,
         Rateing:8.1
     },
     {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
+        ID:37,
+        img:"img/2022/h/PREY for the DEVIL.jpg", 
+        name: "PREY for the DEVIL",
+        catagory:"Horror",
+        year:2022,
+        Rateing:6.6
     },
     {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
+        ID:38,
+        img:"img/2022/h/RETURN.jpg", 
+        name: "RETURN",
+        catagory:"Horror",
+        year:2022,
+        Rateing:7.6
     },
     {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
+        ID:39,
+        img:"img/2022/h/SCREAM.jpg", 
+        name: "SCREAM",
+        catagory:"Horror",
+        year:2022,
+        Rateing:7.3
     },
     {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
-    },
-    {
-        ID:1,
-        img:"hdfdff", 
-        name: "gdfgdfgd",
-        catagory:"DFUYHSG",
-        year:2020,
-        Rateing:8.1
+        ID:40,
+        img:"img/2022/h/The LAST Thing MARY saw.jpg", 
+        name: "The LAST Thing MARY saw",
+        catagory:"Horror",
+        year:2022,
+        Rateing:6.0
     }
+   
 ]
 

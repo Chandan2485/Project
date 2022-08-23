0e69dfdb23da2bf924242d1cac10962c58760902
Chandan2485 <chandan96kar@gmail.com>
2022-08-24 00:10:45 +0530

    hxjvx

---
+++ re.js
@@ -0,0 +1,14 @@
+let obj={
+    name:"hhjkkk",
+    age:"21"
+}
+
+
+// console.log(Object.entries(obj));
+
+function vby (num){
+    console.log(num);
+}
+vby("bhng")
+console.log(vby.arguments);
+// vby.arguments
